cabb28d23486e7480f75890de3e6ac70a355989e
Leandro Mariotti <midiasmart99@gmail.com>
2020-09-22 00:25:44 -0300

    Exercicios Vetor

--- orientacaoObjeto/src/application/Vector.java
+++ orientacaoObjeto/src/application/Vector.java
@@ -8,43 +8,37 @@ import entities.Vetor;
 public class Vector {
 
 	public static void main(String[] args) {
-		// if (vect[i] != null)
+
 		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner (System.in);
-		int n = 0;
-		
-		
-		
+		Scanner sc = new Scanner(System.in);
+
+
 		System.out.println("---- Booking.com > O SEU Site de Hospedagem ----");
-		
-		System.out.printf("How many rooms will be rented?");
-		n = sc.nextInt();
-		Vetor[] vect = new Vetor[n];
-		for(int i = 0; i < vect.length; i++) {
+
+		System.out.printf("How many rooms will be rented? ");
+		int n = sc.nextInt();
+		Vetor[] vect = new Vetor[10];
+		for (int i = 1; i < n; i++) {
+			System.out.println();
+			System.out.println("Rent #" + i + ":");
+			System.out.print("Name: ");
 			sc.nextLine();
-			System.out.printf("Digite seu nome: ");
 			String name = sc.nextLine();
-			System.out.printf("Digite seu email: ");
+			System.out.print("Email: ");
 			String email = sc.nextLine();
-			vect[i] = new Vetor(name, email);
-			vect[5].setName("Maria Green");
-			vect[5].setEmail("maria@gmail.com");
-			vect[1].setName("Marco Antonio");
-			vect[1].setEmail("marco@gmail.com");
-			vect[8].setName("Alex Brown");
-			vect[8].setEmail("alex@gmail.com");
-			if(vect[i] != null) {
-				System.out.println("Busy Rooms:\n");
-				System.out.println("1: "+vect[1].getName() + ", "+vect[1].getEmail());
-				System.out.println("5: "+vect[5].getName()+ ", "+vect[5].getEmail());
-				System.out.println("8: "+vect[8].getName()+ ", "+vect[8].getEmail());
-			}else {
-				System.out.println("erro");
+			System.out.print("Room: ");
+			int room = sc.nextInt();
+			vect[room] = new Vetor(name, email);
+
+		}
+		System.out.println();
+		System.out.println("Busy Rooms:");
+		for (int i = 0; i < 10; i++) {
+			if (vect[i] != null) {
+				System.out.println(i + ": " + vect[i].getName() + ", " + vect[i].getEmail());
 			}
 		}
-		
-		
-		
+
 		sc.close();
 
 	}
